a4afa3edbcfc88f377ed3de834988ac6ca21de4d
raeyoungchang <raeyoungj@gmail.com>
2024-06-21 00:25:37 +0900

    exercise: BJ

---
+++ BaekJoon/B2444.py
@@ -0,0 +1,9 @@
+n = int(input())
+answer = ' '
+for i in range(n) :
+    answer += ' '* (n-i) + '*'*((2*i)-1) + '\n'
+
+for i in range(n-2,-1,-1) :
+    answer += ' ' * (n-i-1) + '*' * (2*i+1) + '\n'
+
+print (answer)
