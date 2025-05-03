778c35562b7761a2fb544faf8713999214f7ec0d
raeyoungchang <raeyoungj@gmail.com>
2025-05-03 13:47:09 -0400

    exercise: BJ

---
+++ Programmers/p181922.py
@@ -0,0 +1,25 @@
+
+
+def solution(arr, queries):
+    answer = []
+    arr = [0, 1, 2, 4, 3]
+    queries = [[0, 4, 1],[0, 3, 2],[0, 3, 3]]
+    for i in range(0, 6):
+        print(i)
+        # for i in range(arr):
+        #     print
+        #     if s, i, e in queries:
+#
+# a
+#     return answer
+
+
+solution([], [])
+solution([], [])
+
+# 10 미만의 자연수에서 3과 5의 배수를 구하면 3, 5, 6, 9이다. 이들의 총합은 23이다.
+# 1,000 미만의 자연수에서 3의 배수와 5의 배수의 총합을 구하라.
+def adslfjasd():
+    a =
+    if a % 3 == 0 and a % 5 == 0 :
+        return
