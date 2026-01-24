26dc4552da73cd7324d6a8b99232cdb9f9c4cba8
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-01-24 20:09:02 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260124/윤년인가/is-leap-year.py
+++ 260124/윤년인가/is-leap-year.py
@@ -2,7 +2,9 @@ Y = int(input())
 
 if Y % 4 == 0 :
     print("true")
-elif Y % 100 == 0 and Y % 400 != 0 :
+elif Y%100 == 0 :
     print("false")
+elif Y%400 != 0 : 
+    print("true")
 elif Y % 4 != 0 :
     print("false")
