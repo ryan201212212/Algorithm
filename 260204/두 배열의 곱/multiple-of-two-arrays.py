322535830abc00a8d9f15b445a856f7f89b7b370
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-02-04 19:19:31 +0900

    Title: 두 배열의 곱, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/두 배열의 곱/multiple-of-two-arrays.py
+++ 260204/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,10 +1,20 @@
 rows = 3
 cols = 3
-matrix1 = [list(map(int, input().split())) for _ in range(rows)]
-matrix2 = [list(map(int, input().split())) for _ in range(rows)]
+
+matrix1 = []
+while len(matrix1) < rows:
+    row = input().split()
+    if row:
+        matrix1.append(list(map(int, row)))
+
+matrix2 = []
+while len(matrix2) < rows:
+    row = input().split()
+    if row:
+        matrix2.append(list(map(int, row)))
 
 for i in range(rows):
-    result= []
+    result = []
     for j in range(cols):
-        print(str(matrix1[i][j] * matrix2[i][j]))
+        result.append(str(matrix1[i][j] * matrix2[i][j]))
     print(" ".join(result))
