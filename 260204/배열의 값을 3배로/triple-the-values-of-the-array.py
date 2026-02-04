0e8589ef9761e6d42f3fecbbba089029ed84b0e3
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-02-04 16:52:23 +0900

    Title: 배열의 값을 3배로, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260204/배열의 값을 3배로/triple-the-values-of-the-array.py
+++ 260204/배열의 값을 3배로/triple-the-values-of-the-array.py
@@ -2,10 +2,7 @@ matrix = [3, 2, 3], [1, 3, 1], [1, 1, 1]
 rows = 3
 cols = 3
 
-new_matrix = [
-    [element * 3 for element in row]
-    for row in matrix
-]
-
-for row in new_matrix:
-    print(" ".join(str(i) for i in row))
+for i in range(rows): 
+    for j in range(cols): 
+        print(matrix[i][j] * 3, end=" ") 
+    print()
