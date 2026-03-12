30f4a1b9914ed5e4acade4ba4e49d35d1ef0f46b
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-12 18:09:27 +0900

    Title: 등급 매기기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/등급 매기기/ranking.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+if N >= 90 :
+    print('A')
+
+elif N >= 80 :
+    print('B')
+
+elif N >= 70 :
+    print('C')
+
+elif N >= 60 :
+    print('D')
+
+elif N < 60 :
+    print('F')
