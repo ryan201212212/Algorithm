def25c5dca881b0ff0a3cf78edf3fb5f530daa0e
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-13 15:17:12 +0900

    Title: 최대 2번의 연산, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 260313/최대 2번의 연산/up-to-2-calculations.py
+++ 260313/최대 2번의 연산/up-to-2-calculations.py
@@ -1,9 +1,9 @@
 a = int(input())
-answer = 0
 
 if a % 2 == 0 :
-    answer=int((a/2))
-if answer % 2 != 0 :
-    answer = int(((answer+1)/2))
+    a = a // 2
 
-print(answer)
+if a % 2 != 0 :
+    a = (a+1)//2
+
+print(a)
