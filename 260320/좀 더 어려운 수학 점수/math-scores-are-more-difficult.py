96926d20ab3c7c73da26f22d4fa6c2eff94da64c
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-20 21:17:53 +0900

    Title: 좀 더 어려운 수학 점수, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260320/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+if a > c :
+    print("A")
+elif a < c :
+    print("B")
+elif a == c and b > d :
+    print("A")
+elif a == c and b < d :
+    print("B")
