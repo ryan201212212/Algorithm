c236ac010fafc3700a78087902d684f9dd533b0c
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-20 21:46:30 +0900

    Title: 남녀노소 구분짓기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260320/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+
+if b >= 19 :
+    if a == 0 :
+        print("MAN")
+    else :
+        print("WOMAN")
+
+else :
+    if a == 0 :
+        print("BOY")
+    else :
+        print("GIRL")
