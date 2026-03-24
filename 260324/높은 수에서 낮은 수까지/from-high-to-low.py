58f663b719fd0ff17b82403393d15f30ac46a5e1
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-24 15:20:16 +0900

    Title: 높은 수에서 낮은 수까지, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260324/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,12 @@
+A,B = map(int, input().split())
+
+if A > B :
+    i = A
+    while i <= B :
+        print(i, end=" ")
+        i -= 1
+elif A < B :
+    i = B
+    while i >= A :
+        print(i, end=" ")
+        i -= 1
