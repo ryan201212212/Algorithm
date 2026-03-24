12c00dcc88985fdd2688f49dd569c9f6dc377612
Raeyoung Chang <68136401+ryan201212212@users.noreply.github.com>
2026-03-24 11:52:02 +0900

    Title: 숫자의 증감, Time: 1081ms, Memory: 80MB, Status: Time Limit Exceed - Codetree

---
+++ 260324/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,14 @@
+C, N = input().split()
+N = int(N)
+
+if C == 'A' :
+    i = 1
+    while i <= N :
+        print(i, end=" ")
+        i += 1
+
+elif C == 'D' :
+    i = 1
+    while i <= N :
+        print(i, end=" ")
+        i -= 1
